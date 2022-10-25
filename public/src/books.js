133dfdc20cd2bf0908cb6619e829f1bb63dfa88a
John Sian <115193516+sianjohn9@users.noreply.github.com>
2022-10-25 12:56:27 -0700

    Update books.js
    putting in codes that got left behind

--- public/src/books.js
+++ public/src/books.js
@@ -1,11 +1,44 @@
-function findAuthorById(authors, id) {}
 
-function findBookById(books, id) {}
+function findAuthorById(authors, id) {
+  let found = authors.find((author) => author.id === id);
+  return found;
+ }
 
-function partitionBooksByBorrowedStatus(books) {}
+function findBookById(books, id) {
+ let foundBooks = books.find((book) => book.id === id);
+ return foundBooks;
+}
 
-function getBorrowersForBook(book, accounts) {}
 
+function partitionBooksByBorrowedStatus(books) {
+  let booksReturned = books.filter((book) =>
+   book.borrows.every((borrow) => borrow.returned === true)
+  );
+
+   
+  let booksBorrowed = books.filter((book) =>
+  book.borrows.some((borrow) => borrow.returned === false)
+ );
+
+  
+ let finalArray = [[...booksBorrowed], [...booksReturned]];
+ return finalArray;
+}
+
+
+
+
+function getBorrowersForBook(book, accounts) {
+  return book.borrows
+   .map((borrow) => {
+    let account = accounts.find((account) => account.id === borrow.id);
+    return { ...borrow, ...account };
+   })
+   .slice(0, 10);
+ }
+
+
+ 
 module.exports = {
   findAuthorById,
   findBookById,
